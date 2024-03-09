c5710411dce80bf5f602515ab0ba843206214a9b
Geralmy Urena <geralmyuri11@gmail.com>
2024-03-09 15:29:35 -0800

    doggenarator done

--- src/Doggenerator.js
+++ src/Doggenerator.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Doggenerator() {
+    const [image,setImage] = useState('https://cdn.britannica.com/79/232779-050-6B0411D7/German-Shepherd-dog-Alsatian.jpg')
+    const [likes,setLikes] = useState(0)
+
+    function handleNewDogClick(){
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(res => res.json())
+        .then(data=> setImage(data.message))
+        setLikes(0)
+    }
+
+function handleLikes(){
+    setLikes(likes+1)
+
+
+}
+
   return (
-    <div>
-        <img src='https://cdn.britannica.com/79/232779-050-6B0411D7/German-Shepherd-dog-Alsatian.jpg'/>
+    <div className='dog'>
+        
+        <img width="300px" height="215px" src={image}/>
+        <button onClick = {handleLikes}>likes {likes}</button>
+        <button onClick={handleNewDogClick}>New Dog</button>
     </div>
   )
 }
